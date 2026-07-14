623068eba0ee4b81834d324d4ac5e1ce4a2d6d8d
aryagaikwad-coditas <arya.gaikwad@coditas.com>
2026-07-14 09:16:14 +0530

    first one

---
+++ java-backend/Backend-ClaimWise/Backend-ClaimWise/src/main/java/com/example/Backend_ClaimWise/BackendClaimWiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.Backend_ClaimWise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendClaimWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendClaimWiseApplication.class, args);
+	}
+
+}
---
+++ java-backend/Backend-ClaimWise/Backend-ClaimWise/src/test/java/com/example/Backend_ClaimWise/BackendClaimWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backend_ClaimWise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendClaimWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
